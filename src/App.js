c6fb7d1f51a189007244c131cd76414bd0937b3f
Jennifer S <jennygentle14@gmail.com>
2023-03-22 20:53:19 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+
 
 function App() {
+  const [author, setAuthor]= useState('');
+  const [quote, setQuote]=useState('');
+  useEffect(()=>{
+    newQuotes()
+  },[]);
+  
+  const newQuotes = ()=>{
+    let allQuotes =`https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json`;
+
+    fetch(allQuotes)
+    .then(res => res.json())
+    .then(data => {
+      let dataQuotes= data.quotes;
+      console.log(dataQuotes);
+      
+      let randomQuote= Math.floor(Math.random()* dataQuotes.length);
+      let newRanQuote= dataQuotes[randomQuote];
+
+      setQuote(newRanQuote.quote);
+      setAuthor(newRanQuote.author);
+    })
+  }
+  const handleClick =() => {
+    newQuotes();
+   }
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quote Generator</h1>
+      <div className="main">
+      <p className="text">{quote}</p>
+      <i className="aut">{author}</i>
+      <button onClick={handleClick}>New Quote</button>
+      </div>
+      
+          
     </div>
-  );
+  )
 }
 
 export default App;
+
